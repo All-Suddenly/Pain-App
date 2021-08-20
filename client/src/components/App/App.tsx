7068a8fa70def185ad48ef6c9825ce4299836799
David Clausen <davidclausen2051@gmail.com>
2021-08-20 17:14:40 -0500

    feat: add some tailwind

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -20,6 +20,20 @@ export function App() {
             <span style={{ fontSize: '8rem' }}>Wow!</span>
           </strong>
         </div>
+
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <img
+              className="h-12 w-12"
+              src={process.env.PUBLIC_URL + '/svgs/chat_icon.svg'}
+              alt="ChitChat Logo"
+            />
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
       </main>
     </div>
   );
