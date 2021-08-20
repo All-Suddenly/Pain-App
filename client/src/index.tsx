3312fb87d1cbc99b179ae2286365564a4f0b689c
David Clausen <davidclausen2051@gmail.com>
2021-08-20 16:24:22 -0500

    feat: add some eslint/prettier

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import { App } from './components/App';
 import './config/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
