323636364ab85f809b5adf70b7a2d0f53dc01a9d
David Clausen <davidclausen2051@gmail.com>
2021-09-08 19:54:55 -0500

    feat: add color config for tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#fff',
+        black: '#111',
+      },
+    },
   },
   variants: {
     extend: {},
